8127315af8864a1a571b8857e26831f2eac592c4
juleenek <leeenek0@gmail.com>
2023-01-21 21:04:30 +0100

    Basic form of walker

--- random_walker/sketch.js
+++ random_walker/sketch.js
@@ -1,7 +1,34 @@
+let x, y;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  x = width / 2;
+  y = height / 2;
+  background(51);
 }
 
 function draw() {
-  background(220);
+  stroke(255, 100);
+  strokeWeight(2);
+  point(x, y);
+
+  const move = floor(random(4));
+  switch (move) {
+    case 0:
+      x = x + 1;
+      break;
+    case 1:
+      x = x - 1;
+      break;
+    case 2:
+      y = y + 1;
+      break;
+    case 3:
+      y = y - 1;
+      break;
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
